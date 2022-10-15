0eb3a01c95abaa5c03a27171d5597d0fec4b8e87
luxygod <90205803+vadim10q@users.noreply.github.com>
2022-10-15 08:18:38 +0300

    Add files via upload

---
+++ maven-demo/src/main/java/com/luxygod/automation/App.java
@@ -0,0 +1,17 @@
+package com.luxygod.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+    
+    public static void doNothing() {
+    	// do nothing
+    }
+}
