ac5dc274c79d11209749358d6bed88c270321586
luxygod <90205803+vadim10q@users.noreply.github.com>
2022-11-12 08:21:00 +0300

    Add files via upload

---
+++ maven-demo/src/test/java/com/luxygod/automation/EmptyClass.java
@@ -0,0 +1,13 @@
+package com.luxygod.automation;
+
+public class EmptyClass {
+	
+	public static void emptyMethod() {
+		// empty
+	}
+	
+	public static void unusedMethod() {
+		// do not use it
+	}
+
+}
